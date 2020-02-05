b12c4d94986f94f99619056c0cc930566cf98251
Bentur <bertrand.turck@ensta-bretagne.org>
2020-02-05 19:19:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,41 +1,43 @@
-echo "salut"
-
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 
 source /opt/ros/kinetic/setup.bash
 
-echo "\n__Création du Workspace"
-rm -rf /home/ue32/workspace_DDBoat
-mkdir /home/ue32/workspace_DDBoat
-mkdir /home/ue32/workspace_DDBoat/src
-cd /home/ue32/workspace_DDBoat
+echo "__Création du Workspace"
+rm -rf /home/ue32/workspaceDDBoat
+mkdir /home/ue32/workspaceDDBoat
+mkdir /home/ue32/workspaceDDBoat/src
+cd /home/ue32/workspaceDDBoat
 catkin_make
 
-echo "source /home/ue32/workspace_DDBoat/devel/setup.bash" >> ~/.bashrc
+echo "source /home/ue32/workspaceDDBoat/devel/setup.bash" >> ~/.bashrc
 
-echo "\n____Création du package test_kalman_command"
-echo "____Création du package gpsd_client"
-cd /home/ue32/workspace_DDBoat/src
+cd /home/ue32/workspaceDDBoat/src
 catkin_create_pkg test_kalman_command --rosdistro=kinetic
 catkin_create_pkg gpsd_client --rosdistro=kinetic
 catkin_create_pkg start --rosdistro=kinetic
 catkin_create_pkg cap_boat --rosdistro=kinetic
 catkin_create_pkg encoders_boat --rosdistro=kinetic
 
+#récupération des codes 
 unset http_proxy 
 unset https_proxy
 
 git clone https://github.com/affraike/Guerledan.git
-cp -r /home/ue32/workspace_DDBoat/src/Guerledan/workspaceDDBoat/src/* .
+cp -r /home/ue32/workspaceDDBoat/src/Guerledan/workspaceDDBoat/src/* .
+rm -rf /home/ue32/workspaceDDBoat/src/Guerledan
 
-rm -rf /home/ue32/workspace_DDBoat/src/Guerledan
+echo " "
+echo "__compilation de catkin"
+source /home/ue32/workspaceDDBoat/devel/setup.bash
+cd /home/ue32/workspaceDDBoat
 
-echo "\n__compilation de catkin"
-source /home/ue32/workspace_DDBoat/devel/setup.bash
-cd /home/ue32/workspace_DDBoat
-catkin_make -j2
+catkin_make --pkg gpsd_client encoders_boat
+catkin_make --pkg start cap_boat
+catkin_make --pkg test_kalman_command
+#catkin_make --cmake-args -Wno-dev 
 
-echo "__Pour lancer, il faut tapper :"
+echo "____________________________________________________________________________________"
+echo "Pour lancer, il faut tapper :"
 echo "roslaunch start general.launch"
 
-#bash
+bash
